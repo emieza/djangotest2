1135d67a752b57bd1498ea6a25b27e6fd71ae6a6
Enric <emieza@xtec.cat>
2024-01-04 17:22:18 +0100

    requirements selenium

--- myapp/tests.py
+++ myapp/tests.py
@@ -34,3 +34,17 @@ class MySeleniumTests(StaticLiveServerTestCase):
         password_input.send_keys('admin123')
         self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
 
+        self.assertEqual( self.selenium.title , "Site administration | Django site admin" )
+
+    def test_login_error(self):
+        # comprovem que amb un usuari i contrasenya inexistent, el test falla
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
+        self.assertEqual( self.selenium.title , "Log in | Django site admin" )
+        username_input = self.selenium.find_element(By.NAME,"username")
+        username_input.send_keys('usuari_no_existent')
+        password_input = self.selenium.find_element(By.NAME,"password")
+        password_input.send_keys('contrasenya_incorrecta')
+        self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
+        # utilitzem assertNotEqual per testejar que NO hem entrat
+        self.assertNotEqual( self.selenium.title , "Site administration | Django site admin" )
+
