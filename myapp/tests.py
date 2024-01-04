26abb9b898a341e92ec31fbb01c62dc8157f705a
Enric <emieza@xtec.cat>
2024-01-04 17:12:34 +0100

    testdb

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+ 
+class MySeleniumTests(StaticLiveServerTestCase):
+    # aqui podem carregar una BD de test
+    fixtures = ['testdb.json',]
+ 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        opts = Options()
+        cls.selenium = WebDriver(options=opts)
+        cls.selenium.implicitly_wait(5)
+ 
+    @classmethod
+    def tearDownClass(cls):
+        # tanquem browser
+        cls.selenium.quit()
+        super().tearDownClass()
+ 
+    def test_login(self):
+        # anem directament a la pàgina d'accés a l'admin panel
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
+ 
+        # comprovem que el títol de la pàgina és el què esperem
+        self.assertEqual( self.selenium.title , "Log in | Django site admin" )
+ 
+        # introduïm dades de login i cliquem el botó "Log in" per entrar
+        username_input = self.selenium.find_element(By.NAME,"username")
+        username_input.send_keys('admin')
+        password_input = self.selenium.find_element(By.NAME,"password")
+        password_input.send_keys('admin123')
+        self.selenium.find_element(By.XPATH,'//input[@value="Log in"]').click()
 
-# Create your tests here.
